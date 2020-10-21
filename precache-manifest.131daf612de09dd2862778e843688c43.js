bebb46872d8ee432593ef0d351c4633833ed6fee
iamnelsonmartins <hi@iamnelsonmartins.com>
2020-10-21 21:56:37 +0000

    Deploying to live from  @ f34da74d3c2deb8d90886bb46ff044575af393be 🚀

---
+++ precache-manifest.131daf612de09dd2862778e843688c43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c7c8cca17352ca50c444b15a91e8093",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0b4bcf6828dd99964a61",
+    "url": "./static/css/main.deadd662.chunk.css"
+  },
+  {
+    "revision": "7f06c117e6622a421e2a",
+    "url": "./static/js/2.5c68c685.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "./static/js/2.5c68c685.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b4bcf6828dd99964a61",
+    "url": "./static/js/main.66b47844.chunk.js"
+  },
+  {
+    "revision": "59cf8f43a8fb08c1c051",
+    "url": "./static/js/runtime-main.3043c9fd.js"
+  }
+]);
